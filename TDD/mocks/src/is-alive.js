004c70e8018a0c8558f22504ab6581931f003b67
washonrails <iamx.3ur@protonmail.com>
2023-07-15 15:04:47 -0300

    style: rename function

--- TDD/mocks/src/is-alive.js
+++ TDD/mocks/src/is-alive.js
@@ -1,8 +1,8 @@
-const checkIsAlive = (characters, name, mockFn) => {
+const isAlive = (characters, name, mockFn) => {
   const character = characters.find(char => char.name.includes(name))
 
   return character && !!character.alive ? mockFn() : false
 }
 
 
-module.exports = checkIsAlive
+module.exports = isAlive
