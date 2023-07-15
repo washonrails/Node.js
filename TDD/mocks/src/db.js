4841d6550405bd13d3d181d3ad94f552937602b7
washonrails <iamx.3ur@protonmail.com>
2023-07-15 15:03:05 -0300

    feat: add fake database

---
+++ TDD/mocks/src/db.js
@@ -0,0 +1,9 @@
+const mockCharacters = [
+ { name: 'anakin skywalker', nickname: 'darth vader', alive: true },
+ { name: 'leia organa', alive: false },
+ { name: 'luke skywalker', alive: false},
+ { name: 'r2-d2', alive: true },
+ { name: 'c3po', alive: true},
+];
+
+module.exports = mockCharacters
