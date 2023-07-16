1c524c321592ae553c6cd42f252e003b4e9de234
washonrails <iamx.3ur@protonmail.com>
2023-07-16 00:01:09 -0300

    refactor: remove describe function

--- TDD/mocks/__mocks__/mock.test.js
+++ TDD/mocks/__mocks__/mock.test.js
@@ -8,52 +8,63 @@ const { default:expect } = require('expect')
 
 (async () => {
 
-  {
-    const result = filterCharacter(mockCharacters, 'skywalker')
-    const expected = 2
-
-    console.log(result, expected)
-
-    deepStrictEqual(result, expected)
-  }
+  // afterEach(() => {
+  //   // restore the spy created with spyOn
+  //   jest.restoreAllMocks()
+  // })
 
   {
-    describe('filterCharacter', () => {
-       it('Should filter a list of chars and return the number of corresponding name on object', () => {
+    it('Should return the number of chars and to be exactly of exepected', () => {
+      const result = filterCharacter(mockCharacters, 'skywalker')
+      const expected = 2
 
-        const result = filterCharacter(mockCharacters, 'skywalker')
-        const expected = 2
-
-        expect(result).toEqual(expected)
-
-    })
-   })
-  }
-
-  {
-    describe('isAlive', () => {
-      it('Should verify if character is alive and must be executed at least one time', () => {
-
-        const mockIsAlive = jest.fn().mockReturnValue(true)
-        const result = isAlive(mockCharacters, 'skywalker', mockIsAlive)
-        const expected = true
-
-        expect(result).toEqual(expected)
-        expect(mockIsAlive).toHaveBeenCalledTimes(1)
-
-      })
+      deepStrictEqual(result, expected)
     })
   }
 
   {
-    describe('spy logger', () => {
-      it('Should make a log from given objs', () => {
-        const spy = jest.spyOn(Logger, 'log')
-        const isLog = Logger.log()
-        const expected = 1
+    it('Should filter a list of chars and return the number of corresponding name on object', () => {
 
-        expect(spy).toHaveBeenCalledTimes(expected)
-      })
+      const result = filterCharacter(mockCharacters, 'skywalker')
+      const expected = 2
+
+      expect(result).toEqual(expected)
+    })
+  }
+
+  {
+    it('Should verify if character is alive and must be executed at least one time', () => {
+
+      const mockIsAlive = jest.fn().mockReturnValue(true)
+      const result = isAlive(mockCharacters, 'skywalker', mockIsAlive)
+      const expected = true
+
+      expect(result).toEqual(expected)
+      expect(mockIsAlive).toHaveBeenCalledTimes(1)
+    })
+  }
+
+  {
+    it('Should make a log and call the function at least one time', () => {
+      const spy = jest.spyOn(Logger, 'log')
+      const isLog = Logger.log()
+      const expected = 1
+
+      expect(spy).toHaveBeenCalledTimes(expected)
+    })
+  }
+
+  {
+    it('Should mock fetch user data function and verify calls of the same', () => {
+      const fetchUserData = jest.fn()
+      fetchUserData.mockReturnValue({ name: 'WALLACE', age: 20 })
+
+      fetchUserData()
+      fetchUserData()
+      console.log(fetchUserData.mock.calls.length)
+
+      fetchUserData.mockClear()
+      console.log(fetchUserData.mock.calls.length)
     })
   }
 
