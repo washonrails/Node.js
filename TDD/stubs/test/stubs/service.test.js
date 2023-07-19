108902a443ab77434697f7c3b3a2e8c2c8d08810
washonrails <iamx.3ur@protonmail.com>
2023-07-18 21:50:08 -0300

    stubs

---
+++ TDD/stubs/test/stubs/service.test.js
@@ -0,0 +1,52 @@
+const Service = require('../../src/service')
+const sinon = require('sinon')
+const { deepStrictEqual } = require('assert')
+const BASE_URL_1 = 'https://swapi.dev/api/planets/1/'
+const BASE_URL_2 = 'https://swapi.dev/api/planets/2/'
+const mocks = {
+  tatooine: require('../mocks/tatooine.json'),
+  alderaan: require('../mocks/alderaan.json')
+}
+;
+(async () => {
+
+   // {
+   //   // vai para a internet !
+   //   const service = new Service()
+   //   const withoutStub = await service.makeRequest(BASE_URL_1)
+   //   console.log(JSON.stringify(withoutStub))
+   // }
+  
+
+   const service = new Service()
+   const stub = sinon.stub(service, service.makeRequest.name)
+
+   stub
+     .withArgs(BASE_URL_1)
+     .resolves(mocks.tatooine)
+   stub
+     .withArgs(BASE_URL_2)
+     .resolves(mocks.alderaan)
+
+   {
+    const expected = {
+      "name": "Tatooine",
+      "surfaceWater": "1",
+      appearedIn: 5
+    }
+    const result = await service.getPlanets(BASE_URL_1)
+    deepStrictEqual(result, expected)
+   }
+
+   {
+    const expected = {
+      "name": "Alderaan",
+      "surfaceWater": "40",
+      appearedIn: 2
+    }
+
+    const result = await service.getPlanets(BASE_URL_2)
+    deepStrictEqual(result, expected)
+   }
+
+})()
